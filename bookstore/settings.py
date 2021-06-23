23ba737bdaa32c40848ecea5099343d5f84878ba
BrianOyollo <oyollobrian@gmail.com>
2021-06-23 09:24:44 +0300

    Signup,login and logout pages.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'homepage'
+LOGOUT_REDIRECT_URL = 'homepage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+from .forms import CustomUserCreationForm
+from .views import SignUpPageView
 
 
 # Create your tests here.
@@ -21,3 +24,27 @@ class CustomUserTests(TestCase):
         self.assertTrue(test_admin.is_active)
         self.assertTrue(test_admin.is_staff)
         self.assertTrue(test_admin.is_superuser)
+
+
+class SignUpPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_page(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there!')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignUpPageView.as_view().__name__
+        )
